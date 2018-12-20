b0d19338b863978f928ce6dbd9f18dbcef770a61
itggot-zackarias-sabirsh <zackarias.sabirsh@elev.ga.ntig.se>
2018-12-20 13:16:34 +0100

    Skiss förbättrad och index.html klar

---
+++ site/js/main.js
@@ -0,0 +1,10 @@
+function forward(event){
+    let forward = document.querySelector(".forward")
+    console.log("hej")
+    forward.classList.toggle(".activate")
+}
+
+function back(event){
+    let back = document.querySelector(".back")
+    back.classList.toggle(".activate")
+}
